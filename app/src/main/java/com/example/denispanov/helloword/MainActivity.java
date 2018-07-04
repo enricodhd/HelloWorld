5ff3a27db492a3551da8e01968c3bfe74d952c20
denis.panov <denis.panov@akvelon.com>
2018-07-04 10:22:00 +0300

    Fixed app key. Added AppCenter events.

--- app/src/main/java/com/example/denispanov/helloword/MainActivity.java
+++ app/src/main/java/com/example/denispanov/helloword/MainActivity.java
@@ -2,9 +2,15 @@ package com.example.denispanov.helloword;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.Map;
+import java.util.HashMap;
 import com.microsoft.appcenter.AppCenter;
 import com.microsoft.appcenter.analytics.Analytics;
 import com.microsoft.appcenter.crashes.Crashes;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +19,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        AppCenter.start(getApplication(), "d39b72f9-d06c-46aa-b604-7fb8478a0a00", Analytics.class, Crashes.class);
+        AppCenter.setLogLevel(Log.VERBOSE);
+        AppCenter.start(getApplication(), "b6d4def7-debe-46e4-88dc-c383d6db5d02", Analytics.class, Crashes.class);
+
+        Analytics.trackEvent("Test empty event");
+
+        Map<String, String> data = new HashMap<>();
+        data.put("Name", "isTest");
+        data.put("Value", "true");
+        Analytics.trackEvent("Test data event", data);
+
+        //Crashes.generateTestCrash();
     }
 }
